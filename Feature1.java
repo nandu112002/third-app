04cc94fca7904126487860d0111ffcbc4e3d47b6
Vijay Suravarapu <nandu112002@gmail.com>
2023-05-01 13:15:46 -0500

    Feature 2 added

--- Feature1.java
+++ Feature1.java
@@ -2,6 +2,7 @@ public class Feature1 {
 
     public static void main(String[] args) {
         System.out.println("Feature 1 added:: ");
+        System.out.println("Feature 2 added:: ");
         System.out.println("Another change made to feature 1");
         System.out.println("Bug fix1");
     }
